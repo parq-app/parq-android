381d53325b3ce8ed0a679a5bc69a68ad0a306276
nickcharles <nickcmorgan@gmail.com>
2016-03-09 11:57:14 -0500

    Add custom layout for each spot item

--- app/src/main/java/com/mmm/parq/adapters/SpotAdapter.java
+++ app/src/main/java/com/mmm/parq/adapters/SpotAdapter.java
@@ -2,11 +2,14 @@ package com.mmm.parq.adapters;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.TextView;
+
+import com.mmm.parq.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,25 +37,28 @@ public class SpotAdapter extends BaseAdapter {
             return 0;
         }
 
-        // create a new ImageView for each item referenced by the Adapter
+        // create a new TextView for each item referenced by the Adapter
         public View getView(int position, View convertView, ViewGroup parent) {
             Log.d("SPOTADAPTER", "This is the beginning of getView and position is: " + position);
-            TextView textView;
+
+            View hostSpotView;
             if (convertView == null) {
                 // if it's not recycled, initialize some attributes
-                textView = new TextView(mContext);
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                hostSpotView = inflater.inflate(R.layout.host_spot_display, null);
             } else {
-                textView = (TextView) convertView;
+                hostSpotView = convertView;
             }
 
             try {
                 JSONObject spotObj = mSpotsArray.getJSONObject(position);
                 JSONObject attrs = spotObj.getJSONObject("attributes");
                 String title = attrs.getString("title");
+                TextView textView = (TextView) hostSpotView.findViewById(R.id.host_spot_item);
                 textView.setText(title);
             } catch (JSONException e) {
                 Log.d("HOST", "Error parsing spots array");
             }
-            return textView;
+            return hostSpotView;
         }
     }
