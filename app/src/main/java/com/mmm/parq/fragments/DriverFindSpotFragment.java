9406c8396cc93b939857c186ea714b5f70c8a215
kenzshelley <kenzshelley@gmail.com>
2016-03-21 00:06:57 -0400

    Ensure that spots are eventually freed if the user leaves the app while it's in the pre-nav state.

--- app/src/main/java/com/mmm/parq/fragments/DriverFindSpotFragment.java
+++ app/src/main/java/com/mmm/parq/fragments/DriverFindSpotFragment.java
@@ -1,5 +1,6 @@
 package com.mmm.parq.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
@@ -10,9 +11,11 @@ import android.widget.Button;
 
 import com.mmm.parq.R;
 import com.mmm.parq.activities.DriverActivity;
+import com.mmm.parq.interfaces.MapController;
 
 public class DriverFindSpotFragment extends Fragment {
     private Button mFindParkingButton;
+    private MapController mCallback;
 
     public DriverFindSpotFragment() {}
 
@@ -20,6 +23,10 @@ public class DriverFindSpotFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_find_spot_driver, container, false);
+
+        // Clear the map
+        mCallback.clearMap();
+        mCallback.removeEndMarker();
 
         mFindParkingButton = (Button) view.findViewById(R.id.findparkingbutton);
         mFindParkingButton.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +38,18 @@ public class DriverFindSpotFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+
+        try {
+            mCallback = (MapController) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement interface");
+        }
+    }
+
 
     private void reserveSpot() {
         // Start the navigation fragment.
